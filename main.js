2ff001ca3ce2568b7a7cb691c6c790b2151ba8cf
denisevieiraoliveira <denise.vieira90@gmail.com>
2024-02-09 16:14:10 -0300

    commit implementando botao

--- main.js
+++ main.js
@@ -96,30 +96,31 @@ const template = document.querySelector('template')
 const corretas = new Set()
 const totalDePerguntas = perguntas.length
 const mostrarTotal = document.querySelector('#acertos span')
+let botao = document.querySelector('#botao');
 mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
 
 for (const item of perguntas) {
     const quizItem = template.content.cloneNode(true)
 
     quizItem.querySelector('h3').textContent = item.pergunta
-    quizItem.querySelector('#quiz-item__numero').textContent = perguntas.indexOf(item) + 1 
+    quizItem.querySelector('#quiz-item__numero').textContent = perguntas.indexOf(item) + 1
 
     for (let resposta of item.respostas) {
         const dt = quizItem.querySelector('dl dt').cloneNode(true)
         dt.querySelector('span').textContent = resposta
         dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
-        
+
         //dt.querySelector('input').setAttribute('value', perguntas.indexOf(item))
         dt.querySelector('input').value = item.respostas.indexOf(resposta)
 
-        dt.querySelector('input').onchange = (event) => {
-            const estaCorreta = event.target.value == item.correta
-            corretas.delete(item)
-            if (estaCorreta) {
-                corretas.add(item)
-            }
-            mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
-        }
+        // dt.querySelector('input').onchange = (event) => {
+        //     const estaCorreta = event.target.value == item.correta
+        //     corretas.delete(item)
+        //     if (estaCorreta) {
+        //         corretas.add(item)
+        //     }
+        //     mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+        // }
 
         quizItem.querySelector('dl').appendChild(dt)
     }
@@ -127,3 +128,14 @@ for (const item of perguntas) {
     quizItem.querySelector('dl dt').remove()
     quiz.appendChild(quizItem)
 }
+
+botao.addEventListener('click', function (event) {
+    for (const item of perguntas) {
+        const estaCorreta = event.target.value == item.correta
+        corretas.delete(item)
+        if (estaCorreta) {
+            corretas.add(item)
+        }
+    }
+    console.log(mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas)
+})
